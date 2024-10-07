9026e5a4c04e2386c643c528bd16e3e83f3a0b87
JotaN98 <jndomingos1998@gmail.com>
2024-10-07 17:15:55 +0100

    taking tokens from file

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,17 @@
 import requests
 
-with open("tokens.gitignore") as f:
-    text = f.readlines()
-    # Configuration for Telegram
-    TELEGRAM_BOT_TOKEN = text[0]  # Replace with your bot's API token
-    TELEGRAM_CHAT_ID = text[1]  # Replace with your chat ID
+
 
 def send_message(message):
     """
     Sends a message to a specified Telegram chat using the Telegram Bot API.
     """
+    with open("tokens.gitignore") as f:
+        text = f.readlines()
+        # Configuration for Telegram
+        TELEGRAM_BOT_TOKEN = text[0]  # BOT API TOKEN
+        TELEGRAM_CHAT_ID = text[1]  # CHAT ID
+
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
     payload = {
         'chat_id': TELEGRAM_CHAT_ID,
