62218dfc0c325eda892549b474d0f3717efd628e
JotaN98 <jndomingos1998@gmail.com>
2024-10-07 22:04:52 +0100

    token acessed in local file

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,8 +7,8 @@ def send_message(message):
     with open("tokens.gitignore") as f:
         text = f.readlines()
         # Configuration for Telegram
-        TELEGRAM_BOT_TOKEN = text[0]  # BOT API TOKEN
-        TELEGRAM_CHAT_ID = text[1]  # CHAT ID
+        TELEGRAM_BOT_TOKEN = text[0].strip()  # BOT API TOKEN
+        TELEGRAM_CHAT_ID = text[1].strip()  # CHAT ID
 
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
     payload = {
