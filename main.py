10854ae95c5e2ff902ec19f9595cd7523e7953d8
JotaN98 <jndomingos1998@gmail.com>
2024-10-08 13:29:41 +0100

    running as intended :)

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ def main():
     #imports devices csv file into dataframe
     devices = pandas.read_csv("URL/DEVICES.csv")
 
-    #executes web scraping on the url
-    for url in devices["URL"]:
-        web_scraping.geodnet(url)
+    #executes web scraping and sends telegram msgs about the state of each device id, location
+    for index,row in devices.iterrows():
+        #webscraping with selenium and geodnet
+        #web_scraping.selenium_geodnet(row["ID"],row["LOCATION"],row["URL"])
+        web_scraping.kadoa(row["ID"],row["LOCATION"],row["URL"],row["KADOA_API_ID"])
 
 
 if __name__ == "__main__":
