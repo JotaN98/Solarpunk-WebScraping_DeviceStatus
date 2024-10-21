2871d442a7ea6cdc03840e8709d6bc41158b5eac
JotaN98 <jndomingos1998@gmail.com>
2024-10-21 21:00:00 +0100

    optimizations

--- log_keeper.py
+++ log_keeper.py
@@ -6,14 +6,13 @@ def report(date):
     try:
         with open("logbook.txt", "r") as logbook:
             nr_devices, nr_errors, nr_warnings = 0,0,0
-            errors, warnings = [],[]
+            warnings = []
 
             for line in logbook:
                 if len(line.split()) > 0 and line.split()[0] == str(date):
                     nr_devices += 1
                     if "error" in line.lower():
                         nr_errors += 1
-                        errors.append(line)
                     elif "warning" in line.lower():
                         nr_warnings += 1
                         warnings.append(line)
@@ -22,15 +21,15 @@ def report(date):
         print(exception_message)
         telegram_bot.send_message(exception_message.splitlines()[0])
 
-    telegram_bot.send_message(f"🤖📝 Report from {date}\nDevices status: {nr_devices-nr_warnings} / {nr_devices} online")
+    telegram_bot.send_message(f"📝 Report from {date}\n✅ Devices status: {nr_devices-nr_warnings} / {nr_devices} online")
 
     if nr_errors > 0:
-        telegram_bot.send_message(f"Couldn't view the popup content on {nr_errors} / {nr_devices} devices")
+        telegram_bot.send_message(f"❌ Couldn't view the popup content on {nr_errors} / {nr_devices} devices")
 
     if nr_warnings > 0:
         for line in warnings:
             message = " ".join(line.split()[2:]).replace("|","\n")
-            telegram_bot.send_message(message)
+            telegram_bot.send_message(f"⚠️ {message}")
 
 def init():
     #stamps the log with current date & time
