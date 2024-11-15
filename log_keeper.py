9c92d423f79fdab26a38ce193d9b133df1408ec6
JotaN98 <jndomingos1998@gmail.com>
2024-11-15 16:08:10 +0000

    reverted changes

--- log_keeper.py
+++ log_keeper.py
@@ -1,14 +1,5 @@
 from datetime import datetime
-from operator import truediv
-
 import telegram_bot
-
-#FORMAT: -- 2024-10-24 08:07:12.457018 --
-def check_date(string, date, hour):
-    if string.split()[1] == str(date) and string.split()[2][:2] == str(hour):
-        return True
-    else:
-        return False
 
 def report(date, hour):
     # Checks errors and warning msgs in logbook and sends a report as a telegram msg
@@ -16,23 +7,15 @@ def report(date, hour):
         with open("logbook.txt", "r") as logbook:
             nr_devices, nr_errors, nr_warnings = 0,0,0
             warnings = []
-            flag = False
-            for line in logbook:
-                #Checks the initialization date and hour
-                if line[:2] == "--" and line.split()[1] == str(date) and line.split()[2][:2] == str(hour):
-                    flag = True
 
-                if flag and len(line) > 0:
+            for line in logbook:
+                if len(line.split()) > 0 and line.split()[0] == str(date):
                     nr_devices += 1
                     if "error" in line.lower():
                         nr_errors += 1
                     elif "warning" in line.lower():
                         nr_warnings += 1
                         warnings.append(line)
-                # Stops reading the logs when finds another init line
-                if flag and line[:2] == "--":
-                    break
-
     except Exception as e:
         exception_message = f"Prob couldn't find logbook.txt file to read from | Exception: {e}"
         print(exception_message)
@@ -54,6 +37,6 @@ def init():
         logbook.write(f"-- {datetime.now()} --\n")
 
 def write_log(text):
-    # records the text in the loogbook.txt file
+    # records the text (including emojis) in the loogbook.txt file
     with open("logbook.txt","a+") as logbook:
         logbook.write(f"{datetime.now()}: {text}\n")
