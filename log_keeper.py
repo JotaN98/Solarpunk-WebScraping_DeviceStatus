6e3b2bf267b64d6512a6046bafe36e00f6ad3adf
JotaN98 <jndomingos1998@gmail.com>
2024-12-11 19:07:09 +0000

    more efficient functions

--- log_keeper.py
+++ log_keeper.py
@@ -1,27 +1,33 @@
 from datetime import datetime
 import telegram_bot
 
-def report(date, hour):
+def report_last_session():
     # Checks errors and warning msgs in logbook and sends a report as a telegram msg
     try:
-        with open("logbook.txt", "r") as logbook:
-            nr_devices, nr_errors, nr_warnings = 0,0,0
-            warnings = []
+        session_delimiter = "-- "
+        nr_devices, nr_errors, nr_warnings = 0,0,0
+        warnings = []
 
-            for line in logbook:
-                if len(line.split()) > 0 and line.split()[0] == str(date):
+        with open("logbook.txt", "r") as file:
+            for line in reversed(list(file)):
+                line = line.strip()
+                if line.startswith(session_delimiter):
+                    # We found the last session delimiter; stop collecting
+                    break
+                if line:
                     nr_devices += 1
                     if "error" in line.lower():
                         nr_errors += 1
                     elif "warning" in line.lower():
                         nr_warnings += 1
                         warnings.append(line)
+
     except Exception as e:
-        exception_message = f"Prob couldn't find logbook.txt file to read from | Exception: {e}"
+        exception_message = f" Exception: {e}"
         print(exception_message)
         telegram_bot.send_message(exception_message.splitlines()[0])
 
-    telegram_bot.send_message(f"📝 Report from {date}\n✅ Devices status: {nr_devices-nr_warnings} / {nr_devices} online")
+    telegram_bot.send_message(f"📝 Report from {datetime.now().date()}\n✅ Devices status: {nr_devices-nr_warnings} / {nr_devices} online")
 
     if nr_errors > 0:
         telegram_bot.send_message(f"❌ Couldn't view the popup content on {nr_errors} / {nr_devices} devices")
@@ -32,7 +38,7 @@ def report(date, hour):
             telegram_bot.send_message(f"⚠️ {message}")
 
 def init():
-    #stamps the log with current date & time
+    #stamps the log with current session "-- date & time --"
     with open("logbook.txt", "a+") as logbook:
         logbook.write(f"-- {datetime.now()} --\n")
 
